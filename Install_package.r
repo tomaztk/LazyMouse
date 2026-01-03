274eb25dfd8ed8cd7f1b491dbbb1a96c27b75b6c
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2026-01-03 15:04:52 +0100

    Update Install_package.r

--- Install_package.r
+++ Install_package.r
@@ -1,13 +1,11 @@
-
 install.packages("remotes")
 remotes::install_github("tomaztk/LazyMouse")
 
 
-
 library(LazyMouse)
 
-lazy_mouse(
+LazyMouse::lazy_mouse(
   number_of_repetitions = 5,      # number of loops
   time_between_repetitions = 10,  # time in seconds between each loop
-  pixels = 150                    # general change of x-axis mouse position
+  pixels = 150                    # random change of x-axis / y-axis mouse position
 )
